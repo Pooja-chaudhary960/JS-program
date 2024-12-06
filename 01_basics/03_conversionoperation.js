28c443fb91f976d170cce58100e4ef613e62ce3b
Pooja-chaudhary960 <pujadahit52@gmail.com>
2024-12-06 09:09:01 +0000

    comparison

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -28,5 +28,37 @@ console.log(typeof stringNumber);
 // 33
 // String
 
+// Opertaions
+let str1 ="hello"
+let str2 = " Pooja"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2+2);
+// console.log(1+2+"2");
+
+//console.log((3+4)*5%3);
+
+
+let gameCounter = 100
+gameCounter ++;
+//console.log(gameCounter);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
 
 
